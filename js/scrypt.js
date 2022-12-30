3b3c9f2c0e088cd01a89d1fb283a59a6914de73b
Luis Brasil <48132029+Capitaozila@users.noreply.github.com>
2022-12-30 16:04:02 -0300

    loop

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,11 +1,16 @@
 const personagem = document.querySelector(".personagem");
+const pipe = document.querySelector(".pipe");
 
 const jump = () => {
-	personagem.classList.add("jump")
+  personagem.classList.add("jump");
 
-	setTimeout(() => {
-		personagem.classList.remove("jump");
-	}, 500);
-}
+  setTimeout(() => {
+    personagem.classList.remove("jump");
+  }, 500);
+};
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+}, 10);
 
 document.addEventListener("keydown", jump);
